f7805b102177cf7fae0250ab995df9995ff0c198
Anushrav Rathi <60292089+anushravrathi@users.noreply.github.com>
2024-02-28 02:04:34 +0530

    Fixed bug where ink color was not changing

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     inkColorInput.addEventListener("change", () => {
+        context.strokeStyle = inkColorInput.value;
     });
 
     bgColorInput.addEventListener("change", () => {
